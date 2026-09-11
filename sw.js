310d9632c8819765ee21874e60853822c4af1f13
juliancamilo2085 <juliancamilo20@gmail.com>
2026-09-11 09:34:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,6 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+  );
+});

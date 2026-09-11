3783dcd933a2d7059d31b16305cc14bcd0ca740a
juliancamilo2085 <juliancamilo20@gmail.com>
2026-09-11 09:26:43 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,2 @@
-const CACHE_NAME = 'space-dodge-v21';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))));
